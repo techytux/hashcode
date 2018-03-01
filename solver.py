4f2d9cf4e5e253b16b7520e30ddcec0fd12088cb
vaibhav.singh <vaibhav.singh@olx.com>
2018-03-01 17:03:05 +0100

    Initial Commit

---
+++ solver.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def get_coordinates(max_slice=6, pizza_size=15):
+    pass
+
+
+
+def main():
+    pizza_array = np.array([['T', 'T', 'T', 'T', 'T'], ['T', 'M', 'M', 'M', 'T'], ['T', 'T', 'T', 'T', 'T']], str)
+    max_slice_size = 6
+    min_ingredient = 1
+    min_dim = 3
+    left_over_dim = 2
+
+
+    for i in range(3):
+        for j in range(5):
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
